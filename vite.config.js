ec1bd1bea59411cdf3f97f9afab5659750319fb6
andreanvaldy <andrefiliate@gmail.com>
2024-12-28 20:04:46 +0700

    jakak

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Ganti dengan nama repository Anda
 export default defineConfig({
+  base: '/tugasredux2.github.io/',
   plugins: [react()],
-})
+});
